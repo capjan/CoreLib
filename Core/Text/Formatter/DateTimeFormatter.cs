8f0b6b5e6a9cf962149fd2d7fc3969ec43cae394
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-03-15 03:23:54 +0100

    Added string formatter for DateTime values

---
+++ Core/Text/Formatter/DateTimeFormatter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Core.Text.Formatter
+{
+    public class DateTimeFormatter : IDateTimeFormatter
+    {
+        public string DateTimeFormat { get; set; } = "dd.MM.yyyy HH:mm:ss.fff";
+        public bool LocalTime { get; set; } = false;
+
+        public string Format(DateTime value)
+        {
+            if (LocalTime)
+                value = value.ToLocalTime();
+
+            return value.ToString(DateTimeFormat);
+        }
+    }
+}
---
+++ Core/Text/Formatter/ITextFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Core.Text.Formatter
+{
+    public interface ITextFormatter<in T>
+    {
+        string Format(T value);
+    }
+
+    public interface IDateTimeFormatter : ITextFormatter<DateTime>
+    {
+        string DateTimeFormat { get; set; }        
+        bool   LocalTime      { get; set; }
+    }
+}
