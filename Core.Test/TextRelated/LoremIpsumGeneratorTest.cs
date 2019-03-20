ae1fd5f7d61014a35969fb24baeaac8683a8fbb9
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-03-20 16:03:37 +0100

    Added an additional unit test for the lorem ipsum generator

--- Core.Test/TextRelated/LoremIpsumGeneratorTest.cs
+++ Core.Test/TextRelated/LoremIpsumGeneratorTest.cs
@@ -1,6 +1,9 @@
-﻿using Core.Extensions.TextRelated;
+﻿using System.IO;
+using Core.Extensions.TextRelated;
+using Core.IO.Impl;
 using Core.Mathematics.Impl;
 using Core.Text.Generator.Impl;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.ObjectModel;
 using Xunit;
 
 namespace Core.Test.TextRelated
@@ -16,6 +19,23 @@ namespace Core.Test.TextRelated
             Assert.Equal("lorem", text);
             text = gen.CreateText(10);
             Assert.Equal("lorem ipsum dolor sit amet stet kasd gubergren tempor ipsum", text);
+        }
+
+        [Fact]
+        public void BasicLoremIpsumGeneratorToFileTest()
+        {
+            var random = new DefaultRandom(12345);
+            var gen    = new LoremIpsumGenerator(random);
+
+            var tmp = new DefaultTempUtil();
+            tmp.UseFile(tempFile =>
+            {
+                using (var writer = new StreamWriter(tempFile))
+                    gen.WriteText(10, writer);
+                var fileContents = File.ReadAllText(tempFile);
+                Assert.Equal("lorem ipsum dolor sit amet stet kasd gubergren tempor ipsum", fileContents);
+
+            });                       
         }
     }
 }
