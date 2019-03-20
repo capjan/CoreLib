77284156beba60d69d58663559cd17baa4c0b16a
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-03-20 15:39:12 +0100

    Added extension method for easier usage of the lorem ipsum generator

---
+++ Core/Extensions/TextRelated/LoremIpsumGeneratorExt.cs
@@ -0,0 +1,23 @@
+﻿
+using System.IO;
+using System.Text;
+using Core.Text.Generator;
+
+namespace Core.Extensions.TextRelated
+{
+    public static class LoremIpsumGeneratorExt
+    {
+        public static string CreateText(this ILoremIpsumGenerator generator, int wordCount)
+        {
+            using (var memoryStream = new MemoryStream())
+            using (var textWriter = new StreamWriter(memoryStream, Encoding.UTF8))
+            {
+                generator.WriteText(wordCount, textWriter);
+                textWriter.Flush();
+                memoryStream.Position = 0;
+                using (var streamReader = new StreamReader(memoryStream, Encoding.UTF8))
+                    return streamReader.ReadToEnd();
+            }
+        }
+    }
+}
