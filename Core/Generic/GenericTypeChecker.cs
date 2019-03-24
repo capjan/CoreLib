c2b3ef76744581cefb3c8f909ed6a7db4a1ec565
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-03-24 17:21:14 +0100

    Added a utility class to make it easy to check if a generic type is part of a given list of types

---
+++ Core/Generic/GenericTypeChecker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Core.Generic
+{
+    /// <summary>
+    /// Makes it easy to check if a generic type is part of a given type list.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class GenericTypeChecker<T>
+    {
+
+        /// <summary>
+        /// Creates a type checker to check if the generic type is a number
+        /// </summary>        
+        public static GenericTypeChecker<T> NumericTypes()
+        {
+            return new GenericTypeChecker<T>( typeof(byte), typeof(sbyte),
+                                              typeof(short), typeof(ushort),
+                                              typeof(int), typeof(uint),
+                                              typeof(long), typeof(ulong),
+                                              typeof(float),
+                                              typeof(double),
+                                              typeof(decimal),
+                                              typeof(BigInteger));
+        }
+
+        public GenericTypeChecker(params Type[] types)
+        {
+            _types = types;
+        }
+
+        public bool IsType()
+        {
+            var type = typeof(T);
+            return _types.Contains(type);
+        }
+
+        private readonly Type[] _types;
+    }
+}
