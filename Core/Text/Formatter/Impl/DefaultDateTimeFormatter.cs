a219a8d3effc5d5e145765502056a0337310b3e9
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-03-26 00:52:26 +0100

    Renamed useLocalTime to just localTime in DefaultDateTimeFormatter

--- Core/Text/Formatter/Impl/DefaultDateTimeFormatter.cs
+++ Core/Text/Formatter/Impl/DefaultDateTimeFormatter.cs
@@ -8,24 +8,27 @@ namespace Core.Text.Formatter.Impl
     {
         public DefaultDateTimeFormatter(
             string format = "dd.MM.yyyy HH:mm:ss.fff", 
-            bool useLocalTime = false, 
+            bool localTime = true, 
             IFormatProvider formatProvider = default)
         {
             DateTimeFormat = format;
-            _useLocalTime = useLocalTime;
+            _localTime = localTime;
             _formatProvider = formatProvider ?? Thread.CurrentThread.CurrentCulture;
         }
 
         public void WriteFormatted(DateTime value, TextWriter writer)
         {
-            if (_useLocalTime)
-                value = value.ToLocalTime();
+            if (value.Kind != DateTimeKind.Utc)
+                throw new ArgumentException($"{nameof(value)} is not of kind UTC");
+
+            if (_localTime)
+                value = value.ToLocalTime();            
 
             writer.Write(value.ToString(DateTimeFormat, _formatProvider));
         }
     
         private readonly IFormatProvider _formatProvider;
-        private readonly bool _useLocalTime;
+        private readonly bool _localTime;
         public string DateTimeFormat { get; set; }
     }
 }
