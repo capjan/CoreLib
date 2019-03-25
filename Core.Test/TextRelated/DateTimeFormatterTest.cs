ebe21fd692f350254c84ff957ebcc5f36f659520
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-03-26 00:55:21 +0100

    Added unit tests for DateTimeFormatter and SeparatorFormatter

---
+++ Core.Test/TextRelated/DateTimeFormatterTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Core.Extensions.TextRelated;
+using Core.Text.Formatter.Impl;
+using Xunit;
+
+namespace Core.Test.TextRelated
+{
+    public class DateTimeFormatterTest
+    {
+        [Fact]
+        public void BasicTest()
+        {            
+            var utcDateTime = new DateTime(1980, 8, 1, 10,15,0, DateTimeKind.Utc);
+            var formatter = new DefaultDateTimeFormatter(localTime: false);
+            Assert.Equal("01.08.1980 10:15:00.000", formatter.FormatToString(utcDateTime));              
+        }
+    }
+}
---
+++ Core.Test/TextRelated/SeparatorFormatterTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Extensions.CollectionRelated;
+using Core.Extensions.TextRelated;
+using Core.Text.Formatter.Impl;
+using Xunit;
+
+namespace Core.Test.TextRelated
+{
+    public class SeparatorFormatterTest
+    {
+        [Fact]
+        public void BasicTest()
+        {
+            Assert.Equal("one, two, three", new []{"one", "two", "three"}.ToSeparatedString());
+        }
+
+        [Fact]
+        public void ExampleTest()
+        {
+            var intArray = new[] {1, 2, 3, 4, 5};
+
+            var formatter = new DefaultSeparatorFormatter<int>(
+                separator: ", ",
+                groupLength: 1,
+                toStringFunc: v => v.ToString(),
+                nullPlaceholder: "");
+
+            formatter.WriteFormatted(intArray, Console.Out);
+            var formatted = formatter.FormatToString(intArray);
+
+            // or use the extension method on any IEnumerable<T>
+            var formattedString = formatter.FormatToString(intArray);
+
+            var formattedViaExtension = intArray.ToSeparatedString();
+
+            Assert.Equal("one, two, three", new []{"one", "two", "three"}.ToSeparatedString());
+        }
+    }
+}
