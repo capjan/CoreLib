1d1b43b8f78b7ab9a904b47b136c2b2ddcead283
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-03-29 00:10:39 +0100

    Added a unit test for multi attempt control flow utility classes.

---
+++ Core.Test/ControlFlowRelated/MultiAttemptActionTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Core.Extensions.ControlFlowRelated;
+using Xunit;
+
+namespace Core.Test.ControlFlowRelated
+{
+    public class MultiAttemptActionTest
+    {
+        [Fact]
+        public void MultiAttemptTest()
+        {
+            var attemptCount = 0;
+            Action action =  () =>
+            {
+                attemptCount++;
+                if (attemptCount <= 3)
+                    throw new InvalidOperationException();         
+            };
+            var multiAction = action.WithMultipleAttempts();
+            multiAction.Invoke();
+            Assert.Equal(4, multiAction.AttemptCount);
+        }        
+    }
+}
