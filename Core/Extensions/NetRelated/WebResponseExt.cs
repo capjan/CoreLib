b47ccbad9a6e54c9dfc95c61fb1c3e1f6dfd482e
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-03-31 21:05:39 +0200

    Added a extension method for an easy dictionary driven access to http header data

---
+++ Core/Extensions/NetRelated/WebResponseExt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace Core.Extensions.NetRelated
+{
+    public static class WebResponseExt
+    {
+        public static Dictionary<string, string> CreateHeaderDictionary(this WebResponse response)
+        {
+            var header = response.Headers;
+            var keys   = response.Headers.AllKeys;
+            return keys.ToDictionary(key => key, key => header.Get(key));
+        }
+    }
+}
