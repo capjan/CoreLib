39b174d77e700c15c7f1729bfef4de8d7b0b3e6a
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-03-31 20:59:24 +0200

    Added parser exception class

---
+++ Core/Parser/ParserException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Core.Parser
+{
+    public class ParserException : Exception
+    {
+        public ParserException(string message) : base(message) {}
+
+        public ParserException(string message, Exception innerException) : base(message, innerException) {}
+    }
+}
