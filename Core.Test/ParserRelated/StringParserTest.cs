e063805daf05d9ec5e391088a29b2ee37150625e
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-03-31 21:04:37 +0200

    Added unit tests for string and word token parser.

---
+++ Core.Test/ParserRelated/StringParserTest.cs
@@ -0,0 +1,40 @@
+﻿using Core.Parser.Impl;
+using Core.Parser.TokenParser;
+using Xunit;
+
+namespace Core.Test.ParserRelated
+{
+    public class StringParserTest
+    {
+        [Fact]
+        public void BasicTest()
+        {
+            var parser = new StringTokenParser();
+            
+            using (var input = ParserInput.CreateFromString("\"Hello World!\""))
+                Assert.Equal("Hello World!", parser.Parse(input));
+            
+            using (var input = ParserInput.CreateFromString("\"\\\\\""))
+                Assert.Equal("\\", parser.Parse(input));
+
+            parser.EscapedCharLookup.Add('t', '\t');
+
+            using (var input = ParserInput.CreateFromString("\"\\tHello\""))
+                Assert.Equal("\tHello", parser.Parse(input));
+
+        }
+
+        [Fact]
+        public void DoubleQuotationTest()
+        {
+            var doubleQuoteStringParser = new StringTokenParser(allowDoubleQuotationEscaping: true);
+            using (var input = ParserInput.CreateFromString("\"abc\"\"123\""))
+                Assert.Equal("abc\"123", doubleQuoteStringParser.Parse(input));
+
+            var singleQuoteParser = new StringTokenParser(allowDoubleQuotationEscaping: true, quotationChar: '\'');
+            using (var input = ParserInput.CreateFromString("'abc''123'"))
+                Assert.Equal("abc'123", singleQuoteParser.Parse(input));
+        }
+
+    }
+}
---
+++ Core.Test/ParserRelated/WordTokenParserTest.cs
@@ -0,0 +1,22 @@
+﻿using Core.Parser.Impl;
+using Core.Parser.TokenParser;
+using Xunit;
+
+namespace Core.Test.ParserRelated
+{
+    public class WordTokenParserTest
+    {
+
+        [Fact]
+        public void BasicTest()
+        {
+            using (var input = ParserInput.CreateFromString("Hello World"))
+            {
+                var parser = new WordTokenParser();
+                Assert.Equal("Hello", parser.Parse(input));
+                input.TryReadChar(out _);
+                Assert.Equal("World", parser.Parse(input));
+            }
+        }
+    }
+}
