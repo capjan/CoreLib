a9863e378e6c6094686916546374e6bce74b2285
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-03-31 21:04:15 +0200

    Started work on string and word token parser

---
+++ Core/Parser/TokenParser/StringTokenParser.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Extensions.TextRelated;
+
+namespace Core.Parser.TokenParser
+{
+    public class StringTokenParser
+    {
+        public StringTokenParser(bool allowDoubleQuotationEscaping = false, char quotationChar = '"', char? escapeChar = '\\')
+        {
+            _allowDoubleQuotationEscaping = allowDoubleQuotationEscaping;
+            _quotation  = quotationChar;
+            _escapeChar = escapeChar;
+            if (_escapeChar.HasValue)
+            {
+                EscapedCharLookup.Add(_escapeChar.Value, _escapeChar.Value);
+                EscapedCharLookup.Add(_quotation, _quotation);
+            }
+        }
+
+        private readonly StringBuilder          _builder = new StringBuilder();
+        private readonly char                   _quotation;
+        private readonly char?                  _escapeChar;
+        public          Dictionary<char, char> EscapedCharLookup { get;  } = new Dictionary<char, char>();
+        private readonly bool _allowDoubleQuotationEscaping;
+
+        public string Parse(IParserInput input)
+        {
+            _builder.Clear();
+
+            if (!input.TryReadChar(out var quotation))
+                
+                if (quotation != _quotation)
+                    throw new ArgumentException($"strings must start with the quotation char '{quotation}' 0x{quotation.ToHexString()}", nameof(input));
+            var done = false;
+
+            while (!done)
+            {
+                if (!input.TryReadChar(out var nextChar))
+                    throw new ParserException("end of input in string error");
+
+                if (nextChar == '\n')
+                    throw new ParserException("unclosed string error");
+                
+                if (nextChar == _quotation)
+                    if (_allowDoubleQuotationEscaping && input.TryPeekChar(out var doubleQuoteEscapeCandidate) && doubleQuoteEscapeCandidate == _quotation)
+                    {
+                        input.ReadLookahead();
+                        _builder.Append(nextChar);
+                    }
+                    else
+                        done = true;
+                    
+                else if (nextChar == _escapeChar && input.TryPeekChar(out var escapeCandidate) && EscapedCharLookup.TryGetValue(escapeCandidate, out var lookupResult))
+                {
+                    input.ReadLookahead();
+                    _builder.Append(lookupResult);
+                }
+                else
+                    _builder.Append(nextChar);
+            }
+
+            return _builder.ToString();
+        }
+
+    }
+}
---
+++ Core/Parser/TokenParser/WordTokenParser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace Core.Parser.TokenParser
+{
+    public class WordTokenParser
+    {
+        public WordTokenParser(Func<char, bool> isValidWordCharFunc = null)
+        {
+            _isValidWordCharFunc = isValidWordCharFunc ?? char.IsLetterOrDigit;
+        }
+
+        public string Parse(IParserInput input)
+        {
+            var sb = new StringBuilder();
+
+            if (!input.TryReadChar(out var ch))
+                throw new ParserException("parser error in word.");
+
+            sb.Append(ch);
+            var done = false;
+
+            do
+            {
+                if (input.TryPeekChar(out ch) && _isValidWordCharFunc(ch))
+                {
+                    input.ReadLookahead();
+                    sb.Append(ch);
+                }
+                else
+                    done = true;
+            } while (!done);
+
+            return sb.ToString();
+        }
+
+        private readonly Func<char, bool> _isValidWordCharFunc;
+    }
+}
