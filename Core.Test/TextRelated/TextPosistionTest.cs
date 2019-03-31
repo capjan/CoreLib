eaed4c02a3dbf98943467eac97d54625037395b7
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-03-31 20:55:33 +0200

    Added unit test for text position interface and extension methods.

---
+++ Core.Test/TextRelated/TextPosistionTest.cs
@@ -0,0 +1,46 @@
+﻿using Core.Extensions.TextRelated;
+using Core.Text;
+using Core.Text.Impl;
+using Xunit;
+
+namespace Core.Test.TextRelated
+{
+    public class TextPositionTest
+    {
+
+        [Fact]
+        public void BasicTextPositionTest()
+        {
+            var pos1 = (ITextPosition) new TextPosition();
+            Assert.Equal(1,pos1.LineNumber);
+            Assert.Equal(1, pos1.ColumnNumber);
+
+            var pos2 = pos1.NextColumn();
+            Assert.Equal(1,pos2.LineNumber);
+            Assert.Equal(2, pos2.ColumnNumber);
+
+            var pos3 = pos2.NextColumn();
+            Assert.Equal(1,pos3.LineNumber);
+            Assert.Equal(3, pos3.ColumnNumber);
+
+            var pos4 = pos3.NextLine();
+            Assert.Equal(2,pos4.LineNumber);
+            Assert.Equal(1, pos4.ColumnNumber);
+        }
+
+        [Fact]
+        public void BasicTextPositionOperatorTest()
+        {
+            var pos1 = (ITextPosition) new TextPosition();
+            Assert.Equal(1,pos1.LineNumber);
+            Assert.Equal(1, pos1.ColumnNumber);
+
+            var pos2 = (ITextPosition) new TextPosition();
+            Assert.Equal(1,pos2.LineNumber);
+            Assert.Equal(1, pos2.ColumnNumber);
+
+            Assert.False(ReferenceEquals(pos1, pos2));
+            Assert.True(pos1.Equals(pos2));
+        }
+    }
+}
