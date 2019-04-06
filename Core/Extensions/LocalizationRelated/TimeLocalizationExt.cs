aef094ff0546c922a186bb57f906b41e539f8627
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-04-06 22:49:19 +0200

    Added extension method for easier usage of the time localization

---
+++ Core/Extensions/LocalizationRelated/TimeLocalizationExt.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Core.Localization;
+
+namespace Core.Extensions.LocalizationRelated
+{
+    public static class TimeLocalizationExt
+    {
+        public static string GetName(this ITimePartLocalization partLoc, int value)
+        {
+            return value == 1 ? partLoc.Singular : partLoc.Plural;
+        }
+
+        public static void WriteValue(this ITimePartLocalization partLoc, TextWriter writer, int value, bool compact)
+        {
+            writer.Write($"{value:n0}");
+            if (compact)
+            {
+                writer.Write(partLoc.Abbreviation);
+                return;
+            }
+            writer.Write($" {partLoc.GetName(value)}");
+        }
+    }
+}
