0bdd2ca1eb837cf29f01b3be2371b77b25aec864
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-04-07 15:00:13 +0200

    Added string extensions Left and Right

---
+++ Core.Test/TextRelated/StringExtensionsTest.cs
@@ -0,0 +1,22 @@
+﻿using Core.Extensions.TextRelated;
+using Xunit;
+
+namespace Core.Test.TextRelated
+{
+    public class StringExtensionsTest
+    {
+        [Fact]
+        public void RightTest()
+        {
+            Assert.Equal("World", "Hello World".Right(5));
+            Assert.Equal("Hello World", "Hello World".Right(100));
+        }
+
+        [Fact]
+        public void LeftTest()
+        {
+            Assert.Equal("Hell", "Hello World".Left(4));
+            Assert.Equal("Hello World", "Hello World".Left(100));
+        }
+    }
+}
---
+++ Core/Extensions/TextRelated/StringExt.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+using System.Text;
+using Core.Enums;
+using Core.Security.Cryptography.Security;
+
+namespace Core.Extensions.TextRelated
+{
+    public static class StringExt
+    {
+        public static MemoryStream ToMemoryStream(this string value, bool writable = false, Encoding encoding = default)
+        {
+            encoding = encoding ?? Encoding.UTF8;
+            return new MemoryStream(encoding.GetBytes(value), writable);
+        }
+
+        public static StringReader ToStringReader(this string value)
+        {
+            return new StringReader(value);
+        }
+
+        // <summary>
+        /// Returns a string containing a specified number of characters from the right side of a string.
+        /// </summary>
+        public static string Right(this string value, int length)
+        {
+            var maxLength = Math.Min(value.Length, length);
+            var startOffset = value.Length - maxLength;
+            return value.Substring(startOffset, maxLength);
+        }
+
+        // <summary>
+        /// Returns a string containing a specified number of characters from the left side of a string.
+        /// </summary>
+        public static string Left(this string value, int length)
+        {
+            return value.Substring(0, Math.Min(value.Length, length));
+        }
+        
+
+        
+    }
+}
