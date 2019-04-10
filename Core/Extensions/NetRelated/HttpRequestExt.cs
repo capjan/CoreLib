8ff3a0047e090552c5d22c796c9f9088d5be8281
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-04-10 13:02:59 +0200

    Added extension methods for HttpWebRequest to handle the response and response steam in a callback based manner

---
+++ Core/Extensions/NetRelated/HttpRequestExt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Core.Extensions.NetRelated
+{
+    public static class HttpRequestExt
+    {
+        public static void HandleResponse(this HttpWebRequest request, Action<HttpWebResponse> callback)
+        {
+            using (var response = (HttpWebResponse) request.GetResponse())
+                callback(response);
+        }
+
+        public static void HandleResponseStream(this HttpWebRequest request, Action<HttpWebResponse, Stream> callback)
+        {
+            using (var response = (HttpWebResponse) request.GetResponse())
+            using (var stream = response.GetResponseStream())
+                callback(response, stream);
+        }
+    }
+}
