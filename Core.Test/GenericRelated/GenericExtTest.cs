26c1f42d135a1ccf9e70c6787d561bb64c19328f
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-04-10 23:14:24 +0200

    Added extension method "In" to make it easy to check if a value is part of a given list.

---
+++ Core.Test/GenericRelated/GenericExtTest.cs
@@ -0,0 +1,15 @@
+﻿using Core.Extensions.GenericsRelated;
+using Xunit;
+
+namespace Core.Test.GenericRelated
+{
+    public class GenericExtTest
+    {
+        [Fact]
+        public void InTest()
+        {
+            Assert.True(6.In(12,3,4,5,6));
+            Assert.False(5.In(1,2,3,4,6));
+        }
+    }
+}
---
+++ Core/Extensions/GenericsRelated/GenericExt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Core.Extensions.GenericsRelated
+{
+    public static class GenericExt
+    {
+        public static bool In<T>(this T value, params T[] values) where T : IEquatable<T>
+        {
+            return values.Any(i => i.Equals(value));
+        }
+    }
+}
