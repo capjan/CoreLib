dbb5ac3edcb3769e9b6e64a564a29335c1e686ac
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-04-10 23:05:41 +0200

    Added extension methods to get a Ipv4 and IPv6 address from a network interface

---
+++ Core/Extensions/NetRelated/NetworkInterfaceExt.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace Core.Extensions.NetRelated
+{
+    public static class NetworkInterfaceExt
+    {
+        public static IPAddress Ipv4Address(this NetworkInterface value)
+        {
+            return value.AddressByFamily(AddressFamily.InterNetworkV6);
+        }
+
+        public static IPAddress IpV6Address(this NetworkInterface value)
+        {
+            return value.AddressByFamily(AddressFamily.InterNetworkV6);
+        }
+
+        public static IPAddress AddressByFamily(this NetworkInterface value, AddressFamily addressFamily)
+        {
+            return value
+                   .GetIPProperties()
+                   .UnicastAddresses
+                   .FirstOrDefault(i=>i.Address.AddressFamily == addressFamily)?.Address;
+        }
+    }
+}
