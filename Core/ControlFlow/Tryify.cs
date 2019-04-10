2b82030a5646e138202e18379fa3369266cd3433
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-04-10 13:00:29 +0200

    Added Tryify utility class to reduce boiler plate code to write TryXxx functions

---
+++ Core/ControlFlow/Tryify.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ControlFlow
+{
+    public class Tryify<T>
+    {
+        public bool TryInvoke(Func<T> callback, out T result)
+        {
+            try
+            {
+                result = callback();
+                return true;
+            }
+            catch (Exception)
+            {
+                result = default;
+                return false;
+            }
+        }
+    }
+}
