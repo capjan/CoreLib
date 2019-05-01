44d3c753145aa0dce66fc3e3e51b6121da6121ac
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-05-01 15:18:46 +0200

    Added a URL Builder

---
+++ Core.Test/NetRelated/UrlQueryBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Core.Net.Impl;
+using Xunit;
+
+namespace Core.Test.NetRelated
+{
+    public class UrlQueryBuilder
+    {
+        [Fact]
+        public void BasicUsage()
+        {
+            var url = new DefaultUrlQueryBuilder("https://www.test.com")
+                      .AddParameter("name", "John Doe")
+                      .AddParameter("mail", "john.doe@domain.com")
+                      .Build();
+
+            Assert.Equal("https://www.test.com?name=John+Doe&mail=john.doe%40domain.com", url);
+        }
+    }
+}
---
+++ Core/Doc/Net/IUrlQueryBuilder.md
@@ -0,0 +1,20 @@
+﻿# IUrlQueryBuilder
+
+This interface makes it easy to create urls with parameters. The keys and values are url encoded.
+
+## Interface
+```csharp
+public interface IUrlQueryBuilder
+{
+    IUrlQueryBuilder AddParameter(string key, string value);
+    string           Build();
+}
+```
+
+## Examples
+
+### Basic usage
+```csharp
+var url = 
+```
+
