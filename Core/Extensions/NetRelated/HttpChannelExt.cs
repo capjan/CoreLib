ed38cb093b50c81da48a823fedd4c81abed34114
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-05-13 08:55:22 +0200

    Added possibility to add credentials for a download

--- Core/Extensions/NetRelated/HttpChannelExt.cs
+++ Core/Extensions/NetRelated/HttpChannelExt.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using System.Net;
 using System.Text;
 using Core.ControlFlow;
 using Core.Net;
@@ -9,10 +9,12 @@ namespace Core.Extensions.NetRelated
 {
     public static class HttpChannelExt
     {
-        public static string DownloadToString(this IHttpChannel channel, string url, Encoding encoding = default)
+        public static string DownloadToString(this IHttpChannel channel, string url, Encoding encoding = default, ICredentials credentials = null)
         {
             encoding = encoding ?? Encoding.UTF8;
             var request = channel.CreateRequest(url);
+            if (credentials != null)
+                request.Credentials = credentials;
             return channel.StreamResponseTo(request, (res, stream) =>
             {
                 using (var reader = new StreamReader(stream, encoding))
@@ -20,9 +22,13 @@ namespace Core.Extensions.NetRelated
             });
         }
 
-        public static IHttpHeader DownloadHeader(this IHttpChannel channel, string url)
+        public static IHttpHeader DownloadHeader(this IHttpChannel channel, string url, ICredentials credentials = null)
         {
             var request = channel.CreateRequest(url);
+            
+            if (credentials != null)
+                request.Credentials = credentials;
+
             request.Method = "HEAD";
             IHttpHeader header = null;
             request.HandleResponse(response =>
