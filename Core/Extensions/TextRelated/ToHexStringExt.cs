f25a4512e1fa711207da83956fccf43347cd4a4a
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-05-13 08:59:22 +0200

    Minor doc xml change

--- Core/Extensions/TextRelated/ToHexStringExt.cs
+++ Core/Extensions/TextRelated/ToHexStringExt.cs
@@ -9,7 +9,7 @@ namespace Core.Extensions.TextRelated
         /// Creates a hexadecimal string representation of the data.
         /// </summary>
         /// <param name="data">data that should be converted to a string</param>
-        /// <param name="upperCase">true if the ABCDEF hex chars should be written uppercase. false if lower case is preferred.</param>
+        /// <param name="upperCase">true for uppercase hex chars. false for lowercase.</param>
         /// <returns></returns>
         public static string ToHexString(this IEnumerable<byte> data, bool upperCase = true)
         {
