d9b6d4eed85cc92ab84f202a005d988548d59405
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-05-22 22:34:25 +0200

    Added possibility to write cli help at a later time

--- Core/Parser/Arguments/OptionAttributes.cs
+++ Core/Parser/Arguments/OptionAttributes.cs
@@ -32,10 +32,10 @@ namespace Core.Parser.Arguments
 
     public class OptionParser<T> where T : CliOptions, new()
     {
-
         private readonly TextWriter _out;
         private readonly TextWriter _err;
         private readonly IAssemblyInfo _assemblyInfo;
+        private OptionSet _optionSet;
 
         public OptionParser(IAssemblyInfo assemblyInfo = null, TextWriter stdOut = null, TextWriter stdErr = null)
         {
@@ -46,85 +46,92 @@ namespace Core.Parser.Arguments
 
         public bool TryParse(IEnumerable<string> args, out T options)
         {
-            var result        = new T();
-                var type          = result.GetType();
-                var allProperties = type.GetProperties();
+            options = null;
 
-                var optionSet = new OptionSet();
+            var result = new T();
+            InitializeOptionSet(result);
 
-                foreach (var propertyInfo in allProperties)
-                {
-                    if (propertyInfo.CanRead && propertyInfo.CanWrite &&
-                        propertyInfo.TryGetAttribute<OptionAttribute>(
-                            out var attribute))
-                    {
-                        var pType = propertyInfo.PropertyType;
-                        if (pType == typeof(bool))
-                            optionSet.Add(
-                                attribute.Prototype, attribute.Description,
-                                v => propertyInfo.SetValue(result, v != null));
-                        else if (pType == typeof(int))
-                            optionSet.Add(
-                                attribute.Prototype, attribute.Description,
-                                v => propertyInfo.SetValue(result, int.Parse(v)));
-                        else if (pType == typeof(long))
-                            optionSet.Add(
-                                attribute.Prototype, attribute.Description,
-                                v => propertyInfo.SetValue(result, int.Parse(v)));
-                        else if (pType == typeof(string))
-                            optionSet.Add(
-                                attribute.Prototype, attribute.Description,
-                                v => propertyInfo.SetValue(result, long.Parse(v)));
-                        else if (pType == typeof(float))
-                            optionSet.Add(
-                                attribute.Prototype, attribute.Description,
-                                v => propertyInfo.SetValue(result, float.Parse(v)));
-                        else if (pType == typeof(double))
-                            optionSet.Add(
-                                attribute.Prototype, attribute.Description,
-                                v => propertyInfo.SetValue(result, double.Parse(v)));
-                        else if (pType == typeof(decimal))
-                            optionSet.Add(
-                                attribute.Prototype, attribute.Description,
-                                v => propertyInfo.SetValue(result, decimal.Parse(v)));
-                    }
-                }
+            try
+            {
+                result.Extra = _optionSet.Parse(args);
+            }
+            catch (Exception ex)
+            {
+                _err.WriteLine(ex.Message);
+                WriteHelp();
+                return false;
+            }
 
-                options = null;
-                try
-                {
-                    result.Extra = optionSet.Parse(args);
-                }
-                catch (Exception ex)
-                {
-                    _err.WriteLine(ex.Message);
-                    WriteHelp(optionSet);
-                    return false;
-                }
+            if (result.ShowHelp)
+            {
+                WriteHelp();
+                return false;
+            }
 
-                if (result.ShowHelp)
-                {
-                    WriteHelp(optionSet);
-                    return false;
-                }
-                if (result.ShowVersion)
-                {
-                    _out.WriteLine(_assemblyInfo.GetVersionSummary());
-                    return false;
-                }
+            if (result.ShowVersion)
+            {
+                _out.WriteLine(_assemblyInfo.GetVersionSummary());
+                return false;
+            }
 
-                options = result;
-                return true;
+            options = result;
+            return true;
         }
 
-        private void WriteHelp(OptionSet optionSet)
+        private void InitializeOptionSet(T result)
         {
+            var type          = result.GetType();
+            var allProperties = type.GetProperties();
+
+            _optionSet = new OptionSet();
+
+            foreach (var propertyInfo in allProperties)
+            {
+                if (!propertyInfo.CanRead || !propertyInfo.CanWrite || !propertyInfo.TryGetAttribute<OptionAttribute>(
+                        out var attribute)) continue;
+
+                var pType = propertyInfo.PropertyType;
+                if (pType == typeof(bool))
+                    _optionSet.Add(
+                        attribute.Prototype, attribute.Description,
+                        v => propertyInfo.SetValue(result, v != null));
+                else if (pType == typeof(int))
+                    _optionSet.Add(
+                        attribute.Prototype, attribute.Description,
+                        v => propertyInfo.SetValue(result, int.Parse(v)));
+                else if (pType == typeof(long))
+                    _optionSet.Add(
+                        attribute.Prototype, attribute.Description,
+                        v => propertyInfo.SetValue(result, int.Parse(v)));
+                else if (pType == typeof(string))
+                    _optionSet.Add(
+                        attribute.Prototype, attribute.Description,
+                        v => propertyInfo.SetValue(result, long.Parse(v)));
+                else if (pType == typeof(float))
+                    _optionSet.Add(
+                        attribute.Prototype, attribute.Description,
+                        v => propertyInfo.SetValue(result, float.Parse(v)));
+                else if (pType == typeof(double))
+                    _optionSet.Add(
+                        attribute.Prototype, attribute.Description,
+                        v => propertyInfo.SetValue(result, double.Parse(v)));
+                else if (pType == typeof(decimal))
+                    _optionSet.Add(
+                        attribute.Prototype, attribute.Description,
+                        v => propertyInfo.SetValue(result, decimal.Parse(v)));
+            }
+        }
+
+        public void WriteHelp()
+        {
+            if (_optionSet == null)
+                throw new InvalidOperationException($"{nameof(WriteHelp)}() must be called after {nameof(TryParse)}()");
             _out.WriteLine();
             _out.WriteLine("Usage:");
             _out.WriteLine($" {_assemblyInfo.Title} [options]");
             _out.WriteLine();
             _out.WriteLine("Options:");
-            optionSet.WriteOptionDescriptions(_out);
+            _optionSet.WriteOptionDescriptions(_out);
         }
     }
 }
