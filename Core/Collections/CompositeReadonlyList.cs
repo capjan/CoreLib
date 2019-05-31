dff3b61040c81ec751d6e1a55646733295a8abcf
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-05-31 02:17:08 +0200

    Added CompositeReadOnlyList<T>

---
+++ Core/Collections/CompositeReadonlyList.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Core.Collections
+{
+    public class CompositeReadOnlyList<T> : IReadOnlyList<T>
+    {
+        #region Fields
+
+        private readonly IReadOnlyList<T> _first;
+        private readonly IReadOnlyList<T> _second;
+
+        #endregion
+
+        public int Count => _first.Count + _second.Count;
+
+        public T this[int index] => index < _first.Count
+            ? _first[index]
+            : _second[index - _first.Count];
+
+        public CompositeReadOnlyList(IReadOnlyList<T> first, IReadOnlyList<T> second)
+        {
+            _first  = first;
+            _second = second;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new CompositeReadOnlyListEnumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #region Helper Classes
+
+        private class CompositeReadOnlyListEnumerator : IEnumerator<T>
+        {
+            #region Fields
+
+            private readonly CompositeReadOnlyList<T> _list;
+
+            private int _i = -1;
+
+            #endregion
+
+            public T Current { get; private set; }
+
+            object IEnumerator.Current => Current;
+
+            public CompositeReadOnlyListEnumerator(CompositeReadOnlyList<T> list)
+            {
+                _list = list;
+            }
+
+            public bool MoveNext()
+            {
+                ++_i;
+                if (_i >= _list.Count) return false;
+                Current = _list[_i];
+                return true;
+            }
+
+            public void Reset()
+            {
+                _i = -1;
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+
+        #endregion
+    }
+}
