f65c10bb78ea39d61a5bfd2ec9c420086826cc23
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-05-31 11:53:11 +0200

    Added extension methods to get a MemberInfo from a Linq expression

---
+++ Core/Extensions/LinqRelated/LinqExpressionExt.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Core.Extensions.LinqRelated
+{
+    public static class LinqExpressionExt
+    {
+        public static MemberInfo GetMember(this LambdaExpression expression)
+        {
+            var memberExp = RemoveUnary(expression.Body) as MemberExpression;
+            return memberExp?.Member;
+        }
+
+        private static Expression RemoveUnary(Expression toUnwrap)
+        {
+            if (toUnwrap is UnaryExpression expression)
+            {
+                return expression.Operand;
+            }
+
+            return toUnwrap;
+        }
+    }
+}
