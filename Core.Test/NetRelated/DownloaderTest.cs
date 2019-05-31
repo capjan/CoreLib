4aa9f76a37811eff9d66a7231e0a21c5fe74d2af
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-05-31 02:56:05 +0200

    Added unit test for iDownloader interface

---
+++ Core.Test/NetRelated/DownloaderTest.cs
@@ -0,0 +1,22 @@
+﻿using Core.Extensions.NetRelated;
+using Core.Net.Impl;
+using Xunit;
+
+namespace Core.Test.NetRelated
+{
+    public class DownloaderTest
+    {
+        [Fact]
+        public void BasicTest()
+        {
+            const string url = "https://www.example.com/";
+
+            var downloader = new DefaultDownloader();
+            if (!downloader.TryDownloadToString(url, out var result))
+            {
+                Assert.True(false, $"download failed. url: {url}");
+            }
+            Assert.Contains("Example Domain", result);
+        }
+    }
+}
