bdff2e762a12242f15d1dfdb8fc647eab0abb312
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-06-24 04:51:01 +0200

    Fixed unit test

--- Core.Test/MathematicsRelated/TestGeoCoordinate.cs
+++ Core.Test/MathematicsRelated/TestGeoCoordinate.cs
@@ -15,7 +15,7 @@ namespace Core.Test.MathematicsRelated
         [Fact]
         public void TestGeoLocationFactory()
         {
-            var factory = new DefaultGeoLocationFactory();
+            var factory = new GeoLocationFactory();
             var geoBerlin = factory.Create(DecimalLatitudeOfBerlin, DecimalLongitudeOfBerlin);
 
             var latBerlin = geoBerlin.LatitudeDMS;
@@ -24,12 +24,10 @@ namespace Core.Test.MathematicsRelated
             Assert.Equal(52, latBerlin.Degrees);
             Assert.Equal(31, latBerlin.Minutes);
             Assert.Equal(7.1, Math.Round(latBerlin.Seconds, 3, MidpointRounding.AwayFromZero));
-           
             
             Assert.Equal(13, lonBerlin.Degrees);
             Assert.Equal(22, lonBerlin.Minutes);
             Assert.Equal(33.924, Math.Round(lonBerlin.Seconds, 3, MidpointRounding.AwayFromZero));
-            
             
             Assert.Equal("N 52° 31' 07.1\"", geoBerlin.LatitudeDMS.WriteToString());
             Assert.Equal("E 13° 22' 33.9\"", geoBerlin.LongitudeDMS.WriteToString());
