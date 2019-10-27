7ce5c2cb8835f4e85977a868b1f536ba17788527
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-10-27 15:26:58 +0100

    Added enumeration for BinaryPrefixes

---
+++ Core/Enums/BinaryUnitPrefix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Core.Enums
+{
+    /// <summary>
+    /// Binary Unit Prefix (often used for file sizes)
+    /// </summary>
+    public enum BinaryUnitPrefix
+    {
+        /// <summary>
+        /// 1024 Byte
+        /// </summary>
+        Kibi,
+        /// <summary>
+        /// 1024 KiB
+        /// </summary>
+        Mebi,
+        /// <summary>
+        /// 1024 MiB
+        /// </summary>
+        Gibi,
+        /// <summary>
+        /// 1024 GiB
+        /// </summary>
+        Tebi,
+        /// <summary>
+        /// 1024 TiB
+        /// </summary>
+        Pebi,
+    }
+}
