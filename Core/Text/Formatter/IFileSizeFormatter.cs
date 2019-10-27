1c351515db155fb646c618970258153af7ee3980
Jan Ruhländer <jan.ruhlaender@gmail.com>
2019-10-27 15:28:58 +0100

    Added interface IFileSizeFormatter

---
+++ Core/Text/Formatter/IFileSizeFormatter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Core.Enums;
+
+namespace Core.Text.Formatter
+{
+    /// <summary>
+    /// File Size Formatter (binary 1 KB = 1024 B)
+    /// </summary>
+    public interface IFileSizeFormatter : IFormattableTextFormatter<long>
+    {
+        /// <summary>
+        /// content between number and prefixed unit. Defaults to a single space 
+        /// </summary>
+        string Delimiter { get; set; }
+
+        /// <summary>
+        /// Force output to the given Unit Prefix, or null for automatic.
+        /// </summary>
+        BinaryUnitPrefix? ForcedUnit { get; set; }
+    }
+}
