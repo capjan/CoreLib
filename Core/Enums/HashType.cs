c7248da7aba452817b35f472a3a7cd088ee431f2
Jan Ruhländer <jan.ruhlaender@gmail.com>
2020-01-18 20:05:41 +0100

    Added xml documentation

--- Core/Enums/HashType.cs
+++ Core/Enums/HashType.cs
@@ -3,9 +3,21 @@ namespace Core.Enums
 {
     public enum HashType
     {
+        /// <summary>
+        /// Placeholder to mark that no algorithm is chosen
+        /// </summary>
         None,
+        /// <summary>
+        /// MD5 - Message-Digest Algorithm 5
+        /// </summary>
         MD5,
+        /// <summary>
+        /// SHA1 - Secure Hash Algorithm 1
+        /// </summary>
         SHA1,
+        /// <summary>
+        /// 
+        /// </summary>
         CRC32
     }
 }
