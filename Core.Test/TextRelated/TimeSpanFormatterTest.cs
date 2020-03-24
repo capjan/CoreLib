360f8fc2035e5d513865ece94936700ba13904dd
Jan Ruhländer <jan.ruhlaender@gmail.com>
2020-03-24 23:24:00 +0100

    Improved TimeSpanFormatter Test

--- Core.Test/TextRelated/TimeSpanFormatterTest.cs
+++ Core.Test/TextRelated/TimeSpanFormatterTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Core.Extensions.TextRelated;
+using Core.Localization.Impl;
+using Core.Text.Formatter;
 using Core.Text.Formatter.Impl;
 using Xunit;
 
@@ -10,12 +12,23 @@ namespace Core.Test.TextRelated
         [Fact]
         public void BasicTest()
         {
-            var formatter = new DefaultTimeSpanFormatter();
+            ITextFormatter<TimeSpan> formatter = new DefaultTimeSpanFormatter();
             Assert.Equal("1 hour, 0 minutes, 0 seconds", formatter.WriteToString(TimeSpan.FromHours(1)));
             Assert.Equal("1 hour, 5 minutes, 1 second", formatter.WriteToString(new TimeSpan(1, 5, 1)));
             Assert.Equal("6 days, 1 hour, 5 minutes, 1 second", formatter.WriteToString(new TimeSpan(6, 1, 5, 1)));
             Assert.Equal("1 week, 1 day, 1 hour, 5 minutes, 1 second", formatter.WriteToString(new TimeSpan(8, 1, 5, 1)));
             Assert.Equal("2 weeks, 1 day, 1 hour, 5 minutes, 1 second", formatter.WriteToString(new TimeSpan(15, 1, 5, 1)));
+        }
+
+        [Fact]
+        public void BasicLocalizationTest()
+        {
+            var formatter = new DefaultTimeSpanFormatter(TimeLocalization.Create("de"));
+            Assert.Equal("1 Stunde, 0 Minuten, 0 Sekunden", formatter.WriteToString(TimeSpan.FromHours(1)));
+            Assert.Equal("1 Stunde, 5 Minuten, 1 Sekunde", formatter.WriteToString(new TimeSpan(1, 5, 1)));
+            Assert.Equal("6 Tage, 1 Stunde, 5 Minuten, 1 Sekunde", formatter.WriteToString(new TimeSpan(6, 1, 5, 1)));
+            Assert.Equal("1 Woche, 1 Tag, 1 Stunde, 5 Minuten, 1 Sekunde", formatter.WriteToString(new TimeSpan(8, 1, 5, 1)));
+            Assert.Equal("2 Wochen, 1 Tag, 1 Stunde, 5 Minuten, 1 Sekunde", formatter.WriteToString(new TimeSpan(15, 1, 5, 1)));
         }
 
         [Fact]
