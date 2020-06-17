e02cb843ce72829267b6213200bf44bb12267166
Jan Ruhländer <jan.ruhlaender@gmail.com>
2020-06-17 17:52:31 +0200

    Added IParser interface

---
+++ Core/Parser/IParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Parser
+{
+    /// <summary>
+    /// Generic parser to parse a string to a given output type. 
+    /// </summary>
+    /// <typeparam name="T">Output Type</typeparam>
+    public interface IParser<T>
+    {
+        /// <summary>
+        /// Parses the given input string to the given output Type. If the input can't be parsed the given fallback value is returned.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="fallback"></param>
+        /// <returns></returns>
+        T ParseOrFallback(string input, T fallback = default);
+    }
+}
