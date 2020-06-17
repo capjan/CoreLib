bd0a154a63ca853926f4803c830b54d1993ded86
Jan Ruhländer <jan.ruhlaender@gmail.com>
2020-06-17 17:52:51 +0200

    Added Integer Parser

---
+++ Core/Parser/Impl/IntegerParser.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Core.Parser.Impl
+{
+    public class IntegerParser : IParser<int>
+    {
+        private const string RegexPattern = @"(?<number>\d+)";
+
+        public int ParseOrFallback(string input, int fallback = default)
+        {
+            if (string.IsNullOrWhiteSpace(input)) return fallback;
+            var m = Regex.Match(input, RegexPattern);
+            return !m.Success ? fallback : int.Parse(m.Groups["number"].Value);
+        }
+    }
+
+    public class OptionalIntParser : IParser<int?>
+    {
+        private const string RegexPattern = @"(?<number>\d+)";
+
+        public int? ParseOrFallback(string input, int? fallback = default)
+        {
+            if (string.IsNullOrWhiteSpace(input)) return fallback;
+            var m = Regex.Match(input, RegexPattern);
+            return !m.Success ? fallback : int.Parse(m.Groups["number"].Value);
+        }
+    }
+    
+}
