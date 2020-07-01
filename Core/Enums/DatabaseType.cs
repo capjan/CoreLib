96d6bffe442fb9e600339d091c3edd52a77b1965
Jan Ruhländer <jan.ruhlaender@gmail.com>
2020-07-01 16:12:20 +0200

    Added DatabaseType and its Parser

---
+++ Core/Enums/DatabaseType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Enums
+{
+    /// <summary>
+    /// Database Type
+    /// </summary>
+    public enum DatabaseType
+    {
+        /// <summary>
+        /// Indicated that no Database is set. Typically Used to indicate an uninitialized state.
+        /// </summary>
+        None,
+        /// <summary>
+        /// SQLite
+        /// </summary>
+        SQLite,
+        /// <summary>
+        /// Microsoft SQL-Server
+        /// </summary>
+        SQLServer,
+        /// <summary>
+        /// MySQL/
+        /// </summary>
+        MySQL,
+        /// <summary>
+        /// PostgreSQL - Open Source Relational Database
+        /// </summary>
+        PostgreSQL
+    }
+}
