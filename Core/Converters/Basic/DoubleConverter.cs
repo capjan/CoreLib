51d9c682e858a3e038b3dcb0e5f1efd6a8fe10a2
Jan Ruhländer <jan.ruhlaender@gmail.com>
2020-07-06 08:55:23 +0200

    fixed: double converter failed to parse german formated numbers

--- Core/Converters/Basic/DoubleConverter.cs
+++ Core/Converters/Basic/DoubleConverter.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Core.Converters.Basic
 {
     public class DoubleConverter: IConverter<string, double>
     {
+        private const string deToInvariantPattern = @"^(?<signs>\s*-\s*)*(?<number>\d+)(?<grouped>(\.\d{3})*)(?<fraction>,\d+)\s*$";
         public double Convert(string input)
         {
-            return double.Parse(input, NumberStyles.Float, CultureInfo.InvariantCulture);
+            var usedInput = input;
+            var m = Regex.Match(input, deToInvariantPattern);
+            if (m.Success)
+            {
+                var sb = new StringBuilder();
+                if (m.Groups["signs"].Success)
+                    sb.Append(m.Groups["signs"].Value);
+                sb.Append(m.Groups["number"].Value);
+                if (m.Groups["grouped"].Success)
+                    sb.Append(m.Groups["grouped"].Value.Replace(".", ""));
+                sb.Append(m.Groups["fraction"].Value.Replace(",", "."));
+                usedInput = sb.ToString();
+            }
+
+            usedInput = usedInput.Replace(",", "");
+            return double.Parse(usedInput, NumberStyles.Float, CultureInfo.InvariantCulture);
         }
     }
 }
