1983c2b680865385f66b60ad97a0b82a94bcf3c3
Jan Ruhländer <jan.ruhlaender@gmail.com>
2020-12-11 09:55:58 +0100

    Added unit test for EnumUtil

---
+++ Core.Test/EnumRelated/EnumUtilTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core.Enums;
+using Xunit;
+
+namespace Core.Test.EnumRelated
+{
+    public class EnumUtilTest
+    {
+        [Fact]
+        public void BasicTest()
+        {
+            var info = EnumUtil.List<System.Environment.SpecialFolder>().OrderBy(i=>i.Name).ToArray();
+            Assert.Equal("AdminTools", info[0].Name);
+            
+        }
+    }
+}
