fc19798e697abad393de067341773c5eb02c0991
Jan Ruhländer <jan.ruhlaender@gmail.com>
2021-06-06 15:49:55 +0200

    test(si-formatter): added unit tests for convenience methods for doubble and the shorening strategy

--- Core.Test/TextRelated/SiFormatterTest.cs
+++ Core.Test/TextRelated/SiFormatterTest.cs
@@ -1,4 +1,7 @@
 using System.Globalization;
+using System.IO;
+using System.Text;
+using Core.Enums;
 using Core.Extensions.TextRelated;
 using Core.Text.Formatter;
 using Xunit;
@@ -13,6 +16,37 @@ namespace Core.Test.TextRelated
             var formatter = new SiFormatter {FormatProvider = CultureInfo.InvariantCulture};
             var std = formatter.WriteToString(144e4m);
             Assert.Equal("1.44 M", std);
+        }
+
+        [Fact]
+        public void DoubleToStringTest()
+        {
+            var    formatter     = new SiFormatter
+            {
+                FormatProvider = CultureInfo.InvariantCulture, 
+                SignificantDecimalPlaces = 1,
+                ShortenStrategy = NumberShortenStrategy.Round
+            };
+            double doubleValue   = 123.4567;
+            var    formattedText = formatter.WriteToString(doubleValue);
+            Assert.Equal("123.5", formattedText);
+        }
+
+        [Fact]
+        public void DoubleWriteTest()
+        {
+            var    formatter   = new SiFormatter
+            {
+                FormatProvider = CultureInfo.InvariantCulture,
+                SignificantDecimalPlaces = 1,
+                ShortenStrategy = NumberShortenStrategy.Round
+            };
+            double doubleValue = 123.4567;
+            var    sb          = new StringBuilder();
+            var    tw          = new StringWriter(sb);
+            formatter.Write(doubleValue, tw);
+            var formattedText = sb.ToString();
+            Assert.Equal("123.5", formattedText);
         }
     }
 }
