18feaf42e66edb17f8fcfd51eb468df43bc25c32
Jan Ruhlaender <jan.ruhlaender@gmail.com>
2022-07-20 01:10:24 +0200

    docs: added xml documentation for IParserInputPredicate

--- Core/Parser/IParserInputPredicate.cs
+++ Core/Parser/IParserInputPredicate.cs
@@ -2,13 +2,16 @@ using System.IO;
 
 namespace Core.Parser;
 
+/// <summary>
+/// Predicate that can be used to check if the parser input is matching it. Typically it is build with a predicate builder.
+/// </summary>
 public interface IParserInputPredicate
 {
     /// <summary>
     /// Executes the given predicate and returns true on success otherwise nothing happens.
     /// </summary>
-    /// <param name="input"></param>
-    /// <param name="writer"></param>
-    /// <returns></returns>
+    /// <param name="input">Input from which characters are read</param>
+    /// <param name="writer">output to be written if the predicate is a match</param>
+    /// <returns>true, if the predicate is a match, otherwise false</returns>
     bool IsMatch(IParserInput input, TextWriter writer);
 }
